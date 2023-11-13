03c0a446b0e821571d4f6eff92f080515e5f99f9
abdelmalek <abdobenali887@gmail.com>
2023-11-13 22:51:15 +0100

    Complete final project - Billboard to Spotify Playlist

--- main.py
+++ main.py
@@ -3,13 +3,16 @@ import requests
 from pprint import pprint
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+import os
 
-spotify_client_id = 'be814b434e0740d8be2bd4ca9c6aa802'
-spotify_secret_client = '769918b5e39a48768fdc043fa14c1621'
-spotify_get_token_url = 'https://accounts.spotify.com/api/token'
-scope = 'playlist-modify-private'
+
+spotify_client_id = os.environ["SPOTIFY_CLIENT_ID"]
+spotify_secret_client = os.environ['SPOTIFY_SECRET_CLIENT']
+
+
 date = input('which year you want? please type the date in this format YYYY-MM-DD\n')
 year = date.split('-')[0]
+scope = 'playlist-modify-private'
 url = "https://www.billboard.com/charts/hot-100"
 data = requests.get(f"{url}/{date}")
 html_data = data.text
@@ -31,5 +34,7 @@ top_100_song_list = [i.getText().strip() for i in song_title]
 
 uri_list = [spotify.search(q=f"track:{i} year:{year}", type='track')['tracks']['items'][0]['uri'] for i in
             top_100_song_list]
+new_playlist = spotify.user_playlist_create(user=user_id, name=f'{date} Billboard 100', public=False, description='pass XD')
+playlist_data = new_playlist['id']
 
-print(uri_list)
+spotify.playlist_add_items(playlist_id=playlist_data, items=uri_list)
